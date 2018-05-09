1b9e10266b761e5afa338e6446d5b84cc7ad84c6
Jose Miguel Montanana <jmmontanana@gmail.com>
2018-05-09 14:12:42 +0200

    Add files via upload

--- api_command_line/demo_admin_appman_curl.sh
+++ api_command_line/demo_admin_appman_curl.sh
@@ -20,27 +20,25 @@
 #0. ########## GLOBAL VARIABLES ###################################
 	BASE_DIR=`pwd`;
 	server="localhost"; 
-	repository_port="8500";
-
-			BLUE="\033[0;34m";
-		LIGHT_GRAY="\033[0;37m";
+	appmanager_port="8500";
+	app=`basename $0`;
+	cd `dirname $0`;
+	BLUE="\033[0;34m";
+	LIGHT_GRAY="\033[0;37m";
 	LIGHT_GREEN="\033[1;32m";
-		LIGHT_BLUE="\033[1;34m";
-		LIGHT_CYAN="\033[1;36m";
-			yellow="\033[1;33m";
-			WHITE="\033[1;37m";
-			RED="\033[0;31m";
-			marron="\033[2;33m";
-		NO_COLOUR="\033[0m";
-			white="\033[0;0m";
-		nyellow=$'\E[1;33m';
-			cyan=$'\E[36m';
-			reset=$'\E[0m';
-			BC=$'\e[4m'; #underline
-			EC=$'\e[0m'; #not underline
-app=`basename $0`;
-cd `dirname $0`;
-
+	LIGHT_BLUE="\033[1;34m";
+	LIGHT_CYAN="\033[1;36m";
+	yellow="\033[1;33m";
+	WHITE="\033[1;37m";
+	RED="\033[0;31m";
+	marron="\033[2;33m";
+	NO_COLOUR="\033[0m";
+	white="\033[0;0m";
+	nyellow=$'\E[1;33m';
+	cyan=$'\E[36m';
+	reset=$'\E[0m';
+	BC=$'\e[4m'; #underline
+	EC=$'\e[0m'; #not underline 
 #0. #### Function Scripts definition ################################
 	verify_reponse()
 	{ 
@@ -62,10 +60,10 @@ cd `dirname $0`;
 	}
 # 1. #################  CHECK if Respository server is running ###############
 	echo "Checking Repository server ...";
-	verify_reponse ${server} ${repository_port};  
+	verify_reponse ${server} ${appmanager_port};  
 # 2. ##################  CHECK if Elasticsearch is running ###############
 	echo "Checking ElasticSearch ...";
-	HTTP_STATUS=$(curl -s http://${server}:${repository_port}/verify_es_connection);
+	HTTP_STATUS=$(curl -s http://${server}:${appmanager_port}/verify_es_connection);
 	if [[ ${HTTP_STATUS} != "200" ]]; then
 		echo "> Not Response from the ElasticSearch Server. Aborting.";
 		exit 1;
@@ -73,23 +71,35 @@ cd `dirname $0`;
 	echo "Done. Response successfully found on ElasticSearch-server address.";
 # 3. ##################  DELETE DATABASE  ###################################
 	echo -e "\n${LIGHT_BLUE}"; 
-	echo "curl -s -XGET http://${server}:${repository_port}/drop_db";
+	echo "curl -s -XGET http://${server}:${appmanager_port}/drop_db";
 	read -p $'Do you wish to \033[1;37mDELETE\033[1;34m the Repository \033[1;37mDB\033[1;34m? (y/n)' confirm; echo -ne "${NO_COLOUR}";
 	if [[ ! ${confirm} = "" ]]; then
-	if [ ${confirm} == 'y' ] || [ ${confirm} == 'Y' ];then
-		curl -s -XGET http://${server}:${repository_port}/drop_db ;
-		echo
-	fi;
+		if [ ${confirm} == 'y' ] || [ ${confirm} == 'Y' ];then
+			HTTP_STATUS=$(curl  --silent --output /dev/null --write-out "%{http_code}" -XGET http://${server}:${appmanager_port}/drop_db);
+			if [[ ${HTTP_STATUS} != "200" ]]; then
+				echo "> Error, can not delete the DBs."; 
+			else
+				echo "> DATABASE DELETED !!";
+			fi;
+			echo
+		fi;
 	fi;
 # 4. ##################  CREATE A NEW DATABASE  ###################################
 	echo -e "\n${LIGHT_BLUE}"; 
-	echo "curl -s -XGET http://${server}:${repository_port}/new_db";
-	read -p $'Press [Enter] key to \033[1;37mCREATE\033[1;34m a new empty Repository \033[1;37mDB\033[1;34m'; echo -ne "${NO_COLOUR}";
-	curl -s -XGET http://${server}:${repository_port}/new_db;
-# 5. ##################  REGISTER A NEW USER ###################################
+	echo "curl -s -XGET http://${server}:${appmanager_port}/new_db";
+	read -p $'Press [Enter] key to \033[1;37mCREATE\033[1;34m a new empty Repository \033[1;37mDB\033[1;34m'; echo -ne "${NO_COLOUR}"; 
+	HTTP_STATUS=$(curl  --silent --output /dev/null --write-out "%{http_code}" -XGET http://${server}:${appmanager_port}/new_db);
+	if [[ ${HTTP_STATUS} != "200" ]]; then
+		echo "> Error, can not create DBs. ";
+		curl  --silent -XGET http://${server}:${appmanager_port}/new_db;
+	else
+		echo "> DATABASE CREATED !!";
+	fi;
+	echo
+	# 5. ##################  REGISTER A NEW USER ###################################
 	echo -e "\n${LIGHT_BLUE}";
-	echo "curl -XPOST http://${server}:${repository_port}/signup?email=\"bob@example.com\"\&pw=\"1234\"";
+	echo "curl -XPOST http://${server}:${appmanager_port}/signup?email=\"bob@example.com\"\&pw=\"1234\"";
 	read -p $'Press [Enter] key to \033[1;37mREGISTER\033[1;34m the example \033[1;37mUSER\033[1;34m'; echo -ne "${NO_COLOUR}";
-	curl -s -H "Content-Type: application/json" -XPOST http://${server}:${repository_port}/signup?email="montana@abc.com"\&pw="new";
+	curl -s -H "Content-Type: application/json" -XPOST http://${server}:${appmanager_port}/signup?email="montana@abc.com"\&pw="new";
 	#We sync, because it may start the next command before this operation completes.
-	curl -s -XGET ${server}:${repository_port}/_flush > /dev/null;
+	curl -s -XGET ${server}:${appmanager_port}/_flush > /dev/null;
